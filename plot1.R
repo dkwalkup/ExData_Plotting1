b9eee5709071957ef78fdecca5d54250fd9a619e
dkwalkup <dollik2005@yahoo.com>
2014-08-10 17:43:08 -0500

    Assignment figures and scripts
    updated plot1 scripts

--- plot1.R
+++ plot1.R
@@ -1,18 +1,21 @@
-##Script to make plot 1
+##Script for histogram of global active power
  
-##Get the data
-power <- read.csv("household_power_consumption.txt", sep = ";", header = TRUE, colClasses=c("character", "character", "numeric", "numeric", 
-"numeric", "numeric", "numeric", "numeric", "numeric"), na.strings="?")
+##Get the data, specified column classes to subset and create histogram 
+power <- read.csv("household_power_consumption.txt", sep = ";", header = TRUE,  
+                colClasses=c("character", "character", "numeric", "numeric", 
+                "numeric", "numeric", "numeric", "numeric", "numeric"), 
+                na.strings="?")
 
-## Subset data
+## Subset data to limit the dates to Feb 1 & 2, 2007
 data1 <- subset(power, Date == "1/2/2007")
 data2 <- subset(power, Date == "2/2/2007")
-powerData <- rbind(data1, data2) ##note here date and time classes are character, no need to change
+powerData <- rbind(data1, data2) 
 
 ##Create histogram with required labels and color
-hist(powerData$Global_active_power, col = "red", xlab = "Global Active Power(kilowatts)",
-     main = "Global Active Power")
+par(mfrow=c(1,1), mar=c(5,4,4,2))
+hist(powerData$Global_active_power, xlab = "Global Active Power(kilowatts)",
+     main = "Global Active Power", col = "red")
 
-##Save a png of the plot
+##Save a png of the plot !!double check file name!!
 dev.copy(png, file = "plot1.png")
 dev.off()

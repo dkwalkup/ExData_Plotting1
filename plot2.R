e32c5f7f06990993a4a3777b35a52d2b833562c8
dkwalkup <dollik2005@yahoo.com>
2014-08-10 17:40:06 -0500

    Assignment figures and scripts
    Cleaned up comments and spacing to make scripts readable, added explicit
    graph parameters to keep from messing up the graphs as they are made.

--- plot2.R
+++ plot2.R
@@ -1,21 +1,26 @@
-## Script for 2nd plot in assigment
-##Get the data
-power <- read.csv("household_power_consumption.txt", sep = ";", header = TRUE, colClasses=c("character", "character", "numeric", "numeric", 
-                                                                                            "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings="?")
+## Script for line plot of global active power over time
 
-## Subset data
+##Get the data, specified column classes to subset
+power <- read.csv("household_power_consumption.txt", sep = ";", header = TRUE, 
+                  colClasses=c("character", "character", "numeric", "numeric", 
+                  "numeric", "numeric", "numeric", "numeric", "numeric"), 
+                  na.strings="?")
+
+## Subset data to limit the dates to Feb 1 & 2, 2007
 data1 <- subset(power, Date == "1/2/2007")
 data2 <- subset(power, Date == "2/2/2007")
-powerData <- rbind(data1, data2) ##note here date and time classes are character, no need to change
+powerData <- rbind(data1, data2) 
 
 ## Add a date time column to use for plotting
-Date_Time <- strptime(paste(powerData$Date, powerData$Time), "%d/%m/%Y %T", tz="")
+Date_Time <- strptime(paste(powerData$Date, powerData$Time), 
+                      "%d/%m/%Y %T", tz="")
 pwrData <- cbind(powerData, Date_Time)
 
-## plot the data
+## plot the data as a line graph
+par(mfrow=c(1,1), mar=c(5,4,4,2))
 plot(pwrData$Date_Time, pwrData$Global_active_power, xlab = "", 
      ylab = "Global Active Power (kilowatts)", type = "l")
 
-##Save a png of the plot
+##Save a png of the plot !!double check file name!!
 dev.copy(png, file = "plot2.png")
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,25 +1,30 @@
-## Script for 2nd plot in assigment
-##Get the data
-power <- read.csv("household_power_consumption.txt", sep = ";", header = TRUE, colClasses=c("character", "character", "numeric", "numeric", 
-                                                                                            "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings="?")
+## Script for line plot for 3 different sub metering values
+##Get the data, specified column classes to subset
+power <- read.csv("household_power_consumption.txt", sep = ";", header = TRUE, 
+                colClasses=c("character", "character", "numeric", "numeric", 
+                "numeric", "numeric", "numeric", "numeric", "numeric"), 
+                na.strings="?")
 
-## Subset data
+## Subset data to limit the dates to Feb 1 & 2, 2007
 data1 <- subset(power, Date == "1/2/2007")
 data2 <- subset(power, Date == "2/2/2007")
-powerData <- rbind(data1, data2) ##note here date and time classes are character, no need to change
+powerData <- rbind(data1, data2) 
 
 ## Add a date time column to use for plotting
-Date_Time <- strptime(paste(powerData$Date, powerData$Time), "%d/%m/%Y %T", tz="")
+Date_Time <- strptime(paste(powerData$Date, powerData$Time), 
+                      "%d/%m/%Y %T", tz="")
 pwrData <- cbind(powerData, Date_Time)
 
-## plot the data
+## plot the data - 3 different lines for each sub-metering with legend
+par(mfrow=c(1,1), mar=c(5,4,4,2))
 plot(pwrData$Date_Time, pwrData$Sub_metering_1, xlab = "", 
      ylab = "Energy sub metering", type = "n")
 points(pwrData$Date_Time, pwrData$Sub_metering_1, type = "l")
 points(pwrData$Date_Time, pwrData$Sub_metering_2, type = "l", col="red")
 points(pwrData$Date_Time, pwrData$Sub_metering_3, type = "l", col="blue")
-legend("topright", pch = "-", col=c("black", "red", "blue"), 
+legend("topright", pch="-", col=c("black", "red", "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-##Save a png of the plot
+
+##Save a png of the plot !!double check file name!!
 dev.copy(png, file = "plot3.png")
 dev.off()
